b97eb1ec756b2e205595ecde3c0a55551c9883a8
superb-sushi <caleb.ang.zx@gmail.com>
2025-01-11 10:38:27 +0800

    Add Firebase API

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4kfnzP6f-dAIODWOa7WXutw5brmuNGCE",
+  authDomain: "h4g25-42aa4.firebaseapp.com",
+  projectId: "h4g25-42aa4",
+  storageBucket: "h4g25-42aa4.firebasestorage.app",
+  messagingSenderId: "125179454215",
+  appId: "1:125179454215:web:8636a56e67b9a168badba2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
