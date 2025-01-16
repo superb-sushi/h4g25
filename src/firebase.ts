507a99d66c63988e41c2114eaf6b373ed5e72a33
superb-sushi <caleb.ang.zx@gmail.com>
2025-01-16 21:33:44 +0800

    Remove Firebase Config Info

--- src/firebase.ts
+++ src/firebase.ts
@@ -9,12 +9,12 @@ import { Voucher } from "./schema/Voucher";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB4kfnzP6f-dAIODWOa7WXutw5brmuNGCE",
-  authDomain: "h4g25-42aa4.firebaseapp.com",
-  projectId: "h4g25-42aa4",
-  storageBucket: "h4g25-42aa4.firebasestorage.app",
-  messagingSenderId: "125179454215",
-  appId: "1:125179454215:web:8636a56e67b9a168badba2"
+  apiKey: "xxx",
+  authDomain: "xxx",
+  projectId: "xxx",
+  storageBucket: "xxx",
+  messagingSenderId: "xxx",
+  appId: "xxx"
 };
 
 // Initialize Firebase
